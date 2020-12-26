f02f9e1a78b74334b38c610f18ca41c6cc269d9f
Alessandro Rodrigues <alessandro.rodrigues@onlineapp.com.br>
2020-12-25 22:02:56 -0300

    creating projects and architecture

---
+++ src/SimpleWebAPI.Infrastructure.Data.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleWebAPI.Infrastructure.Data.Test
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
